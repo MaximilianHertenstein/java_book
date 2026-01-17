774ba0078216f45c2ca6f5dfabc03af70ca23782
MaximilianHertenstein <hert@ghse.de>
2026-01-17 09:17:54 +0100

    first commit

---
+++ src/SUMMARY.md
@@ -0,0 +1,3 @@
+# Summary
+
+- [Records](./records.md)
---
+++ theme/js/thebe-init.js
@@ -0,0 +1,20 @@
+window.thebeConfig = {
+  binderOptions: {
+    repo: "MaximilianHertenstein/java_book",
+    ref: "main",
+  },
+  kernelOptions: {
+    name: "jjava",
+  },
+  codeMirrorConfig: {
+    theme: "default",
+    lineNumbers: true,
+  },
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const script = document.createElement("script");
+  script.src = "https://unpkg.com/thebe@0.9.2/lib/index.js";
+  script.async = true;
+  document.body.appendChild(script);
+});
